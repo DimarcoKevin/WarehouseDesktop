969a872a966592b1b1520831f41695248ca0971d
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-07-23 15:33:06 -0400

    Adding global vars from previous project

---
+++ WarehouseDesktop/Variables/GlobalVariables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Warehouse.Variables {
+    public class GlobalVariables {
+
+        // making a singleton class of Global Variables
+        private GlobalVariables() { }
+        private static GlobalVariables instance = null;
+        public static GlobalVariables Instance {
+            get {
+                if (instance == null) {
+                    instance = new GlobalVariables();
+                }
+                return instance;
+            }
+        }
+
+
+
+        public static string user { get; set; }
+
+        public static string role { get; set; }
+
+        public static readonly string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=Warehouse; Integrated Security=true";
+    }
+}
