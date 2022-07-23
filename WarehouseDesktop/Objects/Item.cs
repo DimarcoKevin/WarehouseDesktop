23662e584b8d2337d6a7bb64c6969c2ac82146e2
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-07-23 15:33:42 -0400

    Carrying over item object from previous project

---
+++ WarehouseDesktop/Objects/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Warehouse.Objects {
+    public class Item {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public string description { get; set; }
+        public int max_per_pallet { get; set; }
+        public float price { get; set; }
+
+        public Item() {
+
+        }
+    }
+}
