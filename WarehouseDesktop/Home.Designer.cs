8ede9aa8de0e5afee7a1dfb5510b58de3c5357ec
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-01 22:05:30 -0400

    Setting up home page

--- WarehouseDesktop/Home.Designer.cs
+++ WarehouseDesktop/Home.Designer.cs
@@ -23,19 +23,37 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.pb_image = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pb_image
+            // 
+            this.pb_image.ErrorImage = null;
+            this.pb_image.Image = ((System.Drawing.Image)(resources.GetObject("pb_image.Image")));
+            this.pb_image.InitialImage = null;
+            this.pb_image.Location = new System.Drawing.Point(153, 84);
+            this.pb_image.Name = "pb_image";
+            this.pb_image.Size = new System.Drawing.Size(121, 136);
+            this.pb_image.TabIndex = 0;
+            this.pb_image.TabStop = false;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1296, 809);
+            this.Controls.Add(this.pb_image);
             this.Name = "Home";
             this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox pb_image;
     }
 }
