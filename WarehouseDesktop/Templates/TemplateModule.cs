63d04934b527f67a335b22dc3dea078049eb4133
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-04 23:35:53 -0400

    Removing all extra imports

--- WarehouseDesktop/Templates/TemplateModule.cs
+++ WarehouseDesktop/Templates/TemplateModule.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WarehouseDesktop.Templates {
+﻿namespace WarehouseDesktop.Templates {
     public partial class TemplateModule : UserControl {
         public TemplateModule() {
             InitializeComponent();
