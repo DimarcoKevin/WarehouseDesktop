4ae0c637391046f918f5c127a1e5565aa3ae0ceb
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-11 21:34:40 -0400

    Setting up favModule constructors and click events

--- WarehouseDesktop/Templates/TemplateFavourites.cs
+++ WarehouseDesktop/Templates/TemplateFavourites.cs
@@ -10,12 +10,30 @@ using System.Windows.Forms;
 
 namespace WarehouseDesktop.Templates {
     public partial class TemplateFavourites : UserControl {
+        private string imagePath { get; set; }
+        private string moduleTitle { get; set; }
+
         public TemplateFavourites() {
             InitializeComponent();
+            imagePath = "Not populated";
+            moduleTitle = "Not populated";
         }
 
-        private void TemplateFavourites_Load(object sender, EventArgs e) {
+        public TemplateFavourites(string title, string image) {
+            InitializeComponent();
+            moduleTitle = title;
+            imagePath = image;
 
+            // works for now but its super hacky
+            string filePath = "../../../Images/";
+
+            // setting image field and title field of object
+            pb.BackgroundImage = Image.FromFile(filePath + imagePath);
+            l_title.Text = moduleTitle;
+        }
+
+        private void ModuleClick(object sender, EventArgs e) {
+            MessageBox.Show("Module " + moduleTitle + " opened!");
         }
     }
 }
