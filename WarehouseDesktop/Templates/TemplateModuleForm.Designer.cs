0874f46eb477c9f11696a03d974fa8d6c8521b57
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-17 22:56:54 -0400

    Adding SearchBox to module template

--- WarehouseDesktop/Templates/TemplateModuleForm.Designer.cs
+++ WarehouseDesktop/Templates/TemplateModuleForm.Designer.cs
@@ -23,13 +23,22 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.templateSearchBox1 = new WarehouseDesktop.Templates.TemplateSearchBox();
             this.SuspendLayout();
+            // 
+            // templateSearchBox1
+            // 
+            this.templateSearchBox1.Location = new System.Drawing.Point(12, 12);
+            this.templateSearchBox1.Name = "templateSearchBox1";
+            this.templateSearchBox1.Size = new System.Drawing.Size(360, 50);
+            this.templateSearchBox1.TabIndex = 0;
             // 
             // TemplateModuleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.templateSearchBox1);
             this.Name = "TemplateModuleForm";
             this.Text = "TemplateModuleForm";
             this.Load += new System.EventHandler(this.TemplateModuleForm_Load);
@@ -38,5 +47,7 @@
         }
 
         #endregion
+
+        private TemplateSearchBox templateSearchBox1;
     }
 }
--- WarehouseDesktop/Templates/TemplateModuleForm.cs
+++ WarehouseDesktop/Templates/TemplateModuleForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WarehouseDesktop.Templates;
 
 namespace WarehouseDesktop.Templates {
     public partial class TemplateModuleForm : Form {
