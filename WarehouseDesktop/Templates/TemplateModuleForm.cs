972797be3b26d24c745c2414e0c0501759f6e3ff
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-17 22:07:47 -0400

    Adding TemplateModuleForm

---
+++ WarehouseDesktop/Templates/TemplateModuleForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarehouseDesktop.Templates {
+    public partial class TemplateModuleForm : Form {
+        public TemplateModuleForm() {
+            InitializeComponent();
+        }
+
+        private void TemplateModuleForm_Load(object sender, EventArgs e) {
+                // Making page full screen 
+                this.TopMost = true;
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+    }
+}
