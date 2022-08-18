aa9854272de5f095990bffc2e6b976eb7e472ed6
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-17 22:54:05 -0400

    Adding textbox and sizing to searchbox object

--- WarehouseDesktop/Templates/TemplateSearchBox.Designer.cs
+++ WarehouseDesktop/Templates/TemplateSearchBox.Designer.cs
@@ -23,18 +23,30 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.SearchBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(16, 12);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(329, 23);
+            this.SearchBox.TabIndex = 0;
             // 
             // TemplateSearchBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SearchBox);
             this.Name = "TemplateSearchBox";
-            this.Size = new System.Drawing.Size(345, 40);
+            this.Size = new System.Drawing.Size(360, 50);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox SearchBox;
     }
 }
