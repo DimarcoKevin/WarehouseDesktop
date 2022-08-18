70707926203f64b1714eb5c497ca379690c6b60a
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-17 23:07:42 -0400

    Setting up template

--- WarehouseDesktop/Templates/TemplateDataGridView.Designer.cs
+++ WarehouseDesktop/Templates/TemplateDataGridView.Designer.cs
@@ -23,10 +23,33 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            components = new System.ComponentModel.Container();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(3, 3);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.RowTemplate.Height = 25;
+            this.DataGridView.Size = new System.Drawing.Size(679, 442);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // TemplateDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DataGridView);
+            this.Name = "TemplateDataGridView";
+            this.Size = new System.Drawing.Size(685, 446);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataGridView DataGridView;
     }
 }
