5a60c86c7e2b38fbd8b994e01138229fcdc4a2d0
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-08-17 22:19:23 -0400

    Adding another module

---
+++ WarehouseDesktop/Modules/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WarehouseDesktop.Templates;
+
+namespace WarehouseDesktop.Modules {
+    public partial class Users : TemplateModuleForm {
+        public Users() {
+            InitializeComponent();
+        }
+
+        private void Users_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
